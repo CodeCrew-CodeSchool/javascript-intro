cf3c998a8a0f4019b59f88b6e98d0089649d974f
DJ <epicmrjones@gmail.com>
2024-02-21 11:28:11 -0600

    event parameter, event types, removeEventListener

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
 window.onload = function() {
-    const Ebony = document.querySelector("button");
+    //variables
+    const clickBtn = document.querySelector("button");
+    const mouseOverButton = document.querySelector("button + button");
+    const keydownInput = document.querySelector("input");
+    const div = document.querySelector("input + div");
 
+    const password = document.querySelector('input[type="password"]');
+
+    
+    //functions
     function random(number) {
-    return Math.floor(Math.random() * (number + 1));
+        return Math.floor(Math.random() * (number + 1));
     }
 
-    function clickAction() {
+    function changeBackground(event) {
+        console.log(event);
+        console.log(event.target);
         const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
-        document.body.style.backgroundColor = rndCol;
+        div.style = "height: 100px; width: 100px;";
+        div.style.backgroundColor = rndCol;
+
+        if (event.target.innerHTML == "Click") {
+            mouseOverButton.removeEventListener("mouseout", changeBackground);
+            mouseOverButton.removeEventListener("mouseover", changeBackground);
+        }
+        // document.body.style.opacity = 0.25; DO NOT DO. IM SORRY. FORGIVE ME
     }
-    Ebony.addEventListener("click", clickAction);
+
+    //call functions aka execute aka run functions
+    password.addEventListener("focus", (event) => {
+        event.target.style.background = "pink";
+      });
+      
+    password.addEventListener("blur", (event) => {
+        event.target.style.background = "";
+    });
+
+    clickBtn.addEventListener("dblclick", changeBackground);
+    mouseOverButton.addEventListener("mouseover", changeBackground);
+    mouseOverButton.addEventListener("mouseout", changeBackground);
+    keydownInput.addEventListener("keydown", changeBackground);
+
+    
+
 }
 
 /* using function names as variables
